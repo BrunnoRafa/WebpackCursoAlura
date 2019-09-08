8e9478759a45c02565b8787ca06c4329222b8ce4
Bruno Rafael Filgueira Dias <brunor_17@hotmail.com>
2019-09-08 20:10:54 -0300

    continuação das aulas sobre webpack do curso na Alura

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -3,9 +3,13 @@ import { Negociacao } from './domain/index.js';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'bootstrap/js/modal.js';
 import '../css/style.css';
 
-const controller = new NegociacaoController();
+$('h1').on('click',() => alert('você me clicou'));
+console.log($('h1').modal);
+
+const scontroller = new NegociacaoController();
 const negociacao = new Negociacao(new Date(), 1, 200);
 const headers = new Headers();
 headers.set('Content-Type', 'application/json');
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,31 +2,67 @@ const path = require('path');
 const babiliWebpackPlugin = require('babili-webpack-plugin');
 const extractTextWebpackPlugin = require('extract-text-webpack-plugin');
 const optimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 let plugins = [];
 
+plugins.push(
+    new HtmlWebpackPlugin({
+        hash: true,
+        minify: {
+            html5: true,
+            collapseWhitespace: true,
+            removeComments: true
+        },
+        filename: 'index.html',
+        template: __dirname + '/index.html'
+    })
+);
+
 plugins.push(new extractTextWebpackPlugin('style.css'));
 
-if (process.env.NODE_ENV == 'production') {
-    plugins.push(new babiliWebpackPlugin());
+plugins.push(
+    new webpack.ProvidePlugin({
+        '$': 'jquery/dist/jquery.js',
+        'jQuery': 'jquery/dist/jquery.js'
+    })
+);
 
-    plugins.push(new optimizeCssAssetsWebpackPlugin({
-        cssProcessor: require('cssnano'),
-        cssProcessorOptions: {
-            discardComments: {
-                removeAll: true
-            }
-        },
-        canPrint: true
-    }))
+plugins.push(
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor', 
+        filename: 'vendor.bundle.js'
+    })
+);
+
+if (process.env.NODE_ENV == 'production') {
+    plugins.push(new webpack.optimize.ModuleConcatenationPlugin());
+    plugins.push(new babiliWebpackPlugin());
+    plugins.push(
+        new optimizeCssAssetsWebpackPlugin({
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: {
+                discardComments: {
+                    removeAll: true
+                }
+            },
+            canPrint: true
+        })
+    );
 }
 
 module.exports = {
-    entry: './app-src/app.js',
+    entry: {
+        app: './app-src/app.js',
+        vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: 'dist'
+    },
+    devServer: {
+        port: 4400
     },
     module: {
         rules: [
